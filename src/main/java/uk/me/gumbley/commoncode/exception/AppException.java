54af2341c78b00b57b3fe76fecf09312ce8a4027
matt <unknown>
2008-04-12 21:04:07 +0000

    Added AppException

---
+++ src/main/java/uk/me/gumbley/commoncode/exception/AppException.java
@@ -0,0 +1,19 @@
+package uk.me.gumbley.commoncode.exception;
+
+public final class AppException extends Exception {
+    public AppException() {
+        super();
+    }
+
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AppException(Throwable cause) {
+        super(cause);
+    }
+}
