a6ced11042f3792ed85a00294db1f2b353d04489
matt <unknown>
2008-06-04 20:48:33 +0000

    Javadoc and coding conventions adherence

--- src/main/java/uk/me/gumbley/commoncode/file/INIFile.java
+++ src/main/java/uk/me/gumbley/commoncode/file/INIFile.java
@@ -15,8 +15,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
 
+/**
+ * API for access to Windows-.ini style files.
+ * 
+ * @author matt
+ *
+ */
 public class INIFile {
-    private static Logger myLogger = Logger.getLogger(INIFile.class);
+    private static final Logger LOGGER = Logger.getLogger(INIFile.class);
 
     private HashMap < String, Properties> mySectionProperties;
 
@@ -26,26 +32,40 @@ public class INIFile {
 
     private int myWriteSuspensions = 0;
     
+    /**
+     * Create a new .ini file, or load an existing one with a given path.
+     * @param fileName the path of the file to create.
+     */
     public INIFile(final String fileName) {
         super();
         myWriteSuspensions = 0;
         mySectionProperties = new HashMap < String, Properties> ();
         myFile = new File(fileName);
         if (myFile.exists()) {
-            myLogger.debug("Loading existing INI file: " + fileName);
+            LOGGER.debug("Loading existing INI file: " + fileName);
             loadFile();
         } else {
-            myLogger.debug("Creating new INI file: " + fileName);
+            LOGGER.debug("Creating new INI file: " + fileName);
             saveFile();
         }
         bDirty = false;
     }
     
-    public synchronized void suspendWrite() {
-        myWriteSuspensions ++;
+    /**
+     * Allow the writing of the file to be suspended. Calls to this can
+     * be nested, although all such calls must be matched with calls
+     * to resumeWrite, else the file will never be updated.
+     */
+    public final synchronized void suspendWrite() {
+        myWriteSuspensions++;
     }
-    public synchronized void resumeWrite() {
-        myWriteSuspensions --;
+    
+    /**
+     * Resume the writing of the file on calls to setXXXX. If the file had
+     * changed during write suspension, it is now written. 
+     */
+    public final synchronized void resumeWrite() {
+        myWriteSuspensions--;
         if (myWriteSuspensions == 0 && bDirty) {
             saveFile();
         }
@@ -81,7 +101,7 @@ public class INIFile {
                 fos.close();
             }
         } catch (IOException e) {
-            myLogger.error("Could not write INI file " + myFile.getAbsolutePath() + ": " + e.getMessage());
+            LOGGER.error("Could not write INI file " + myFile.getAbsolutePath() + ": " + e.getMessage());
         }
     }
 
@@ -98,7 +118,7 @@ public class INIFile {
             try {
                 while (true) {
                     String line = br.readLine();
-                    myLogger.debug("Read line '" + line + "'");
+                    LOGGER.debug("Read line '" + line + "'");
                     if (line == null) {
                         break;
                     }
@@ -106,7 +126,7 @@ public class INIFile {
                     sectionMatcher.reset(line);
                     if (sectionMatcher.lookingAt()) {
                         currentSectionName = sectionMatcher.group(1);
-                        myLogger.debug("Found section [" + currentSectionName + "]");
+                        LOGGER.debug("Found section [" + currentSectionName + "]");
                         Properties newSectionProperties = new Properties();
                         currentSectionProperties = newSectionProperties;
                         mySectionProperties.put(currentSectionName, newSectionProperties);
@@ -114,126 +134,195 @@ public class INIFile {
                         nvpMatcher.reset(line);
                         if (nvpMatcher.lookingAt()) {
                             if (currentSectionProperties == null) {
-                                myLogger.error("Line " + lineNo + " name=value line not under any [section]: '" + line  + "'");
+                                LOGGER.error("Line " + lineNo + " name=value line not under any [section]: '" + line  + "'");
                             } else {
                                 String name = nvpMatcher.group(1);
                                 String value = nvpMatcher.group(2);
-                                myLogger.debug("[" + currentSectionName + "] " + name + "=" + value);
+                                LOGGER.debug("[" + currentSectionName + "] " + name + "=" + value);
                                 currentSectionProperties.put(name, value);
                             }
                         } else {
-                            myLogger.error("Line " + lineNo + " not matched against [section] or name=value: '" + line + "'");
+                            LOGGER.error("Line " + lineNo + " not matched against [section] or name=value: '" + line + "'");
                         }
                     }
                 }
             } catch (IOException ioe) {
-                myLogger.error("Could not load INI file: " + ioe.getMessage());
+                LOGGER.error("Could not load INI file: " + ioe.getMessage());
             } finally {
                 if (br != null) {
                     try {
                         br.close();
                     } catch (IOException e1) {
-                        myLogger.error("Could not close BufferedReader: " + e1.getMessage());
+                        LOGGER.error("Could not close BufferedReader: " + e1.getMessage());
                     }
                 }
             }
         } catch (FileNotFoundException e) {
-            myLogger.error("INI file " + myFile.getAbsolutePath() + " not found");
+            LOGGER.error("INI file " + myFile.getAbsolutePath() + " not found");
         }
     }
 
+    /**
+     * Obtain a value from the file, with no default if it does not exist.
+     * @param sectionName the [section name] under which this value could be found
+     * @param name the name= key to the given value
+     * @return the value, if one exists, null if it does not exist.
+     */
     public final String getValue(final String sectionName, final String name) {
         if (!mySectionProperties.containsKey(sectionName)) {
-            myLogger.debug("getValue(" + sectionName + "," + name + "): not found [section]");
+            LOGGER.debug("getValue(" + sectionName + "," + name + "): not found [section]");
             return null;
         } else {
             Properties sectionProperties = (Properties) mySectionProperties.get(sectionName);
             String value = (String) sectionProperties.get(name); // returns null on 'not found'
-            myLogger.debug("getValue(" + sectionName + "," + name + "): returning '" + value + "'");
+            LOGGER.debug("getValue(" + sectionName + "," + name + "): returning '" + value + "'");
             return value;
         }
     }
 
+
+    /**
+     * Obtain a value from the file, with a default if it does not exist.
+     * @param sectionName the [section name] under which this value could be found
+     * @param name the name= key to the given value
+     * @param defaultValue the value, if the name does not exist.
+     * @return the value, if one exists, defaultValue if it does not exist.
+     */
     public final String getValue(final String sectionName, final String name, final String defaultValue) {
         String value = getValue(sectionName, name);
         return value == null ? defaultValue : value;
     }
 
+    /**
+     * Remove a name=value pair from the file. If the removed entry is the final
+     * entry in this [section name], remove the section name also.
+     * @param sectionName the [section name] under which the name will be deleted.
+     * @param name the name= that will be deleted, if it exists.
+     */
     public final synchronized void removeValue(final String sectionName, final String name) {
         if (!mySectionProperties.containsKey(sectionName)) {
-            myLogger.debug("removeValue(" + sectionName + ", " + name + "): not found [section]");
+            LOGGER.debug("removeValue(" + sectionName + ", " + name + "): not found [section]");
             return;
         }
         bDirty = true;
         Properties sectionProperties = (Properties) mySectionProperties.get(sectionName);
         sectionProperties.remove(name);
         if (sectionProperties.size() == 0) {
-            myLogger.debug("removeValue(" + sectionName + ", " + name
+            LOGGER.debug("removeValue(" + sectionName + ", " + name
                     + "): final name returned from [section]; removing [section]");
             mySectionProperties.remove(sectionName);
         }
         saveFile();
     }
 
+
+    /**
+     * Store a String value in the file.
+     * @param sectionName the [section name] under which this value will be stored.
+     * @param name the name= key to the given value
+     * @param value the value.
+     */
     public final synchronized void setValue(final String sectionName, final String name, final String value) {
         Properties sectionProperties = null;
         bDirty = true;
         if (!mySectionProperties.containsKey(sectionName)) {
             sectionProperties = new Properties();
             mySectionProperties.put(sectionName, sectionProperties);
-            myLogger.debug("setValue(" + sectionName + "," + name + "," + value + "): created new [class]");
+            LOGGER.debug("setValue(" + sectionName + "," + name + "," + value + "): created new [class]");
         } else {
             sectionProperties = (Properties) mySectionProperties.get(sectionName);
         }
-        myLogger.debug("setValue(" + sectionName + "," + name + "," + value + "): saving");
+        LOGGER.debug("setValue(" + sectionName + "," + name + "," + value + "): saving");
         sectionProperties.put(name, value);
         saveFile();
     }
     
+    /**
+     * Store a long value in the file.
+     * @param sectionName the [section name] under which this value will be stored.
+     * @param name the name= key to the given value
+     * @param value the value.
+     */
     public final void setLongValue(final String sectionName, final String name, final long value) {
         setValue(sectionName, name, "" + value);
     }
 
+    /**
+     * Store an integer value in the file.
+     * @param sectionName the [section name] under which this value will be stored.
+     * @param name the name= key to the given value
+     * @param value the value.
+     */
     public final void setIntegerValue(final String sectionName, final String name, final int value) {
         setValue(sectionName, name, "" + value);
     }
 
+    /**
+     * Store a boolean value in the file.
+     * @param sectionName the [section name] under which this value will be stored.
+     * @param name the name= key to the given value
+     * @param value the value.
+     */
     public final void setBooleanValue(final String sectionName, final String name, final boolean value) {
         setValue(sectionName, name, value ? "TRUE" : "FALSE");
     }
 
+    /**
+     * Obtain a long value from the file.
+     * @param sectionName the [section name] from which this value will be obtained.
+     * @param name the name= key to the given value
+     * @return the value.
+     */
     public final long getLongValue(final String sectionName, final String name) {
         String value = getValue(sectionName, name, "0");
         try {
             return Long.parseLong(value);
         } catch (NumberFormatException e) {
-            myLogger.warn("Value of section [" + sectionName + "], name '" + name + "' is '" + value + "' which is not a long integer");
+            LOGGER.warn("Value of section [" + sectionName + "], name '" + name + "' is '" + value + "' which is not a long integer");
             return 0L;
         }
     }
 
+    /**
+     * Obtain an integer value in the file.
+     * @param sectionName the [section name] from which this value will be obtained.
+     * @param name the name= key to the given value
+     * @return the value.
+     */
     public final int getIntegerValue(final String sectionName, final String name) {
         String value = getValue(sectionName, name, "0");
         try {
             return Integer.parseInt(value);
         } catch (NumberFormatException e) {
-            myLogger.warn("Value of section [" + sectionName + "], name '" + name + "' is '" + value + "' which is not an integer");
+            LOGGER.warn("Value of section [" + sectionName + "], name '" + name + "' is '" + value + "' which is not an integer");
             return 0;
         }
     }
     
+    /**
+     * Obtain a boolean value in the file.
+     * @param sectionName the [section name] from which this value will be obtained.
+     * @param name the name= key to the given value
+     * @return the value.
+     */
     public final boolean getBooleanValue(final String sectionName, final String name) {
         String value = getValue(sectionName, name, "FALSE");
         try {
             return Boolean.parseBoolean(value);
         } catch (NumberFormatException e) {
-            myLogger.warn("Value of section [" + sectionName + "], name '" + name + "' is '" + value + "' which is not a boolean");
+            LOGGER.warn("Value of section [" + sectionName + "], name '" + name + "' is '" + value + "' which is not a boolean");
             return false;
         }
     }
+
+    /**
+     * Obtain an array of values from a section name in the file.
+     * @param sectionName the [section name] from which these values will be obtained.
+     * @return the values.
+     */
     public final String[] getArray(final String sectionName) {
         if (!mySectionProperties.containsKey(sectionName)) {
-            myLogger.debug("getArray(" + sectionName + "): not found [section]");
+            LOGGER.debug("getArray(" + sectionName + "): not found [section]");
             return new String[0];
         } else {
             Properties sectionProperties = (Properties) mySectionProperties.get(sectionName);
@@ -241,24 +330,33 @@ public class INIFile {
         }
     }
     
+    /**
+     * Set an array of values under a section name.
+     * @param sectionName the [section name]
+     * @param array the array of values
+     */
     public final synchronized void setArray(final String sectionName, final String[] array) {
         bDirty = true;
         Properties arrayProperties = new Properties();
-        for (int i=0; i< array.length; i++) {
-            arrayProperties.put("" + (i+1), array[i]);
+        for (int i = 0; i < array.length; i++) {
+            arrayProperties.put("" + (i + 1), array[i]);
         }
         mySectionProperties.put(sectionName, arrayProperties);
-        myLogger.debug("setArray(" + sectionName + ", ...): saving");
+        LOGGER.debug("setArray(" + sectionName + ", ...): saving");
         saveFile();
     }
     
+    /**
+     * Completely remove a [section name], even if populated.
+     * @param sectionName the [section name]
+     */
     public final synchronized void removeSection(final String sectionName) {
         if (!mySectionProperties.containsKey(sectionName)) {
-            myLogger.debug("removeSection(" + sectionName + "): not found [section]");
+            LOGGER.debug("removeSection(" + sectionName + "): not found [section]");
         } else {
             bDirty = true;
             mySectionProperties.remove(sectionName);
-            myLogger.debug("removeSection(" + sectionName + "): saving");
+            LOGGER.debug("removeSection(" + sectionName + "): saving");
             saveFile();
         }
     }
