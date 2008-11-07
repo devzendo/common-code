677ed3b89d2e284e6649840b0ab875bf3fb62f2b
matt <unknown>
2008-11-07 21:45:39 +0000

    Added resource loading code.

---
+++ src/main/java/uk/me/gumbley/commoncode/resource/ResourceLoader.java
@@ -0,0 +1,48 @@
+package uk.me.gumbley.commoncode.resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.log4j.Logger;
+
+/**
+ * Toolkit for helping with loading resources.
+ * 
+ * @author matt
+ *
+ */
+public final class ResourceLoader {
+    private static final Logger LOGGER = Logger.getLogger(ResourceLoader.class);
+    /**
+     * It's a toolkit; no instances
+     */
+    private ResourceLoader() {
+        // no instances
+    }
+
+    /**
+     * Read a resource into a StringBuilder
+     * @param store the StringBuilder to be populated with the resource
+     * @param resourceName the name of the resource, from the classpath 
+     */
+    public static void readResource(final StringBuilder store, final String resourceName) {
+        final InputStream resourceAsStream = Thread.currentThread().
+            getContextClassLoader().
+            getResourceAsStream(resourceName);
+        final int bufsize = 16384;
+        final byte[] buf = new byte[bufsize];
+        int nread;
+        try {
+            while ((nread = resourceAsStream.read(buf, 0, bufsize)) != -1) {
+                final String block = new String(buf, 0, nread);
+                store.append(block);
+            }
+        } catch (final IOException e) {
+            LOGGER.warn("Could not read resource '" + resourceName + "': " + e.getMessage());
+        } finally {
+            try {
+                resourceAsStream.close();
+            } catch (final IOException ioe) {
+            }
+        }
+    }
+}
---
+++ src/test/java/uk/me/gumbley/commoncode/resource/TestResourceLoader.java
@@ -0,0 +1,35 @@
+package uk.me.gumbley.commoncode.resource;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import uk.me.gumbley.commoncode.CCTestCase;
+
+
+/**
+ * Test the resource loader
+ * @author matt
+ *
+ */
+public final class TestResourceLoader extends CCTestCase {
+    private static final Logger LOGGER = Logger
+            .getLogger(TestResourceLoader.class);
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Logger getLogger() {
+        return LOGGER;
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void canGetResource() {
+        final StringBuilder sb = new StringBuilder();
+        ResourceLoader.readResource(sb, "resourceloader.txt");
+        Assert.assertEquals("this is a test", sb.toString());
+    }
+}
